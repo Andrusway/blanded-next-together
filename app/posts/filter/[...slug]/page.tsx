b605b64d8a3eb10cc2d170ace444c0389535b03e
Andrii_Petrukh <andriypetruch2004@gmail.com>
2025-12-06 15:19:41 +0200

    gfcgf

--- app/posts/filter/[...slug]/page.tsx
+++ app/posts/filter/[...slug]/page.tsx
@@ -1,5 +1,8 @@
 // import { fetchPosts } from '@/lib/api';
 
-export default async function PostsPage() {
+export default async function PostsPage({ params }) {
+  const { slug } = await params;
+  console.log(slug);
+
   return <>Клієнтський компонент для сторінки постів</>;
 }
